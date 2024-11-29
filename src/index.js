43995977140c2af0558a3bdae684b8fe6b7429ff
banks-dey <espatb@gmail.com>
2024-11-29 01:15:47 +0000

    added src

---
+++ src/index.js
@@ -0,0 +1,47 @@
+//Store your name inside a variable and log it to the console.
+
+let name = Patience;
+console.log(name);
+
+//Store your age inside a variable and log it to the console.
+let age = 25;
+console.log(25);
+
+//Add two numbers and store it in a variable and log it to the console.
+
+let sum = 5 + 5;
+console.log(sum);
+
+//Subtract two numbers and store it in a variable and log it to the console.
+let difference = 32 - 25;
+console.log(difference);
+
+//Divide two numbers and store it in a variable and log it to the console.
+
+let division = 20 / 5;
+console.log(divison);
+
+//Multiply two numbers and store it in a variable and log it to the console.
+
+let product = 5 * 8;
+console.log(product);
+
+//Find the remainder of 15%4 and store it in a variable and log it to the console.
+
+let remainder = 15 % 4;
+console.log(remainder);
+
+//Store a boolean value inside a variable and log it to the console.
+let isSleeping = true;
+console.log(isSleeping);
+
+//Set the value of a variable to number and log the type of data it is to the console.
+
+let numberValue = 52;
+console.log(typeof numberValue);
+
+
+//Set the value of a variable to a string and log the type of data it is to the console.
+
+let stringValue = "Hello, World🙋‍♀️";
+console.log(typeof stringValue);
